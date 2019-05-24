a8d20bb3c90a5da37f3f239fa2c6593023626aae
khanhafizurrahman80 <khanhafizurrahman80@gmail.com>
2019-05-24 13:44:19 +0200

    project initialized, mysql docker configuration done, data src config is added, domain package is created

---
+++ doctorsappointment-data/src/main/java/com/example/riad/doc_appointment/domains/Person.java
@@ -0,0 +1,8 @@
+package com.example.riad.doc_appointment.domains;
+
+public class Person {
+
+//
+
+
+}
---
+++ doctorsappointment-web/src/main/java/com/example/riad/doctorsappointment/DoctorsappointmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.riad.doctorsappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsappointmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorsappointmentApplication.class, args);
+    }
+
+}
