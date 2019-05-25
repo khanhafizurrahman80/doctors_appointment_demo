59af7646a053f086779e362c411ef75b5d7be9e4
khanhafizurrahman80 <khanhafizurrahman80@gmail.com>
2019-05-25 18:13:03 +0200

    Doctor full name is added

---
+++ doctorsappointment-data/src/main/java/com/example/riad/doctorsappointment/data/domains/DoctorFullName.java
@@ -0,0 +1,15 @@
+package com.example.riad.doctorsappointment.data.domains;
+
+public class DoctorFullName {
+    private String fullName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    
+}
