d4c23be0bbf1c282c256d5678f69dae28f44453f
khanhafizurrahman80 <khanhafizurrahman80@gmail.com>
2019-06-13 13:10:32 +0200

    DoctorControllerTest class is added

---
+++ doctorsappointment-web/src/test/java/com/example/riad/doctorsappointment/web/controllers/DoctorControllerTest.java
@@ -0,0 +1,41 @@
+package com.example.riad.doctorsappointment.web.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class DoctorControllerTest {
+
+    @Test
+    public void getAllDoctors() {
+    }
+
+    @Test
+    public void countAllDoctors() {
+    }
+
+    @Test
+    public void countAllDoctorsDetails() {
+    }
+
+    @Test
+    public void deleteByFirstName() {
+    }
+
+    @Test
+    public void getFirstName() {
+    }
+
+    @Test
+    public void getShortDesc() {
+    }
+
+    @Test
+    public void getIndividualDesc() {
+    }
+
+    @Test
+    public void addDoctor() {
+    }
+}
