81b82d1f9a3e5243622a83894dcec193d4e5e0fd
khanhafizurrahman80 <khanhafizurrahman80@gmail.com>
2019-06-15 22:25:04 +0200

    service test class are added

--- doctorsappointment-data/src/test/java/com/example/riad/doctorsappointment/data/services/AppointmentServiceImplTest.java
+++ doctorsappointment-data/src/test/java/com/example/riad/doctorsappointment/data/services/AppointmentServiceImplTest.java
@@ -2,28 +2,28 @@ package com.example.riad.doctorsappointment.data.services;
 
 import com.example.riad.doctorsappointment.data.repos.AppointmentRepository;
 import com.example.riad.doctorsappointment.data.services.Interfaces.AppointmentService;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 
-//@ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class AppointmentServiceImplTest {
 
-//    @Mock
-//    AppointmentRepository appointmentRepository;
-//
-//    @InjectMocks
-//    AppointmentService appointmentService;
-//
-//    @Test
-//    public void getAllAppointments() {
-//        appointmentService.getAllAppointments();
-//    }
-//
-//    @Test
-//    public void addAppointment() {
-//    }
+    @Mock
+    AppointmentRepository appointmentRepository;
+
+    @InjectMocks
+    AppointmentService appointmentService;
+
+    @Test
+    public void getAllAppointments() {
+        appointmentService.getAllAppointments();
+    }
+
+    @Test
+    public void addAppointment() {
+    }
 }
--- doctorsappointment-data/src/test/java/com/example/riad/doctorsappointment/data/services/DoctorServiceImplTest.java
+++ doctorsappointment-data/src/test/java/com/example/riad/doctorsappointment/data/services/DoctorServiceImplTest.java
@@ -5,36 +5,36 @@ import com.example.riad.doctorsappointment.data.domains.DoctorDetails;
 import com.example.riad.doctorsappointment.data.repos.DoctorRepository;
 import com.example.riad.doctorsappointment.data.services.Interfaces.DoctorService;
 import jdk.nashorn.internal.ir.annotations.Ignore;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashSet;
 import java.util.Set;
 
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-//@ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class)
 class DoctorServiceImplTest {
 
-//    @Mock
-//    DoctorRepository doctorRepository;
-//
-//    @InjectMocks
-//    DoctorService doctorService;
-//
-//    @Test
-//    @Ignore
-//    void getAllDoctors() {
-//        doctorService.getAllDoctors();
-//    }
-//
-//    @Test
-//    void addDoctor() {
-//
-//    }
+    @Mock
+    DoctorRepository doctorRepository;
+
+    @InjectMocks
+    DoctorService doctorService;
+
+    @Test
+    @Ignore
+    void getAllDoctors() {
+        doctorService.getAllDoctors();
+    }
+
+    @Test
+    void addDoctor() {
+
+    }
 
 
 }
