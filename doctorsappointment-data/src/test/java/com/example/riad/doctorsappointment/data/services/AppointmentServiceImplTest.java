5d4b0e25e12ebe9b9baefbd70fde4b37e0d87af5
khanhafizurrahman80 <khanhafizurrahman80@gmail.com>
2019-07-01 12:34:43 +0200

    appointment service impl has been modified

--- doctorsappointment-data/src/test/java/com/example/riad/doctorsappointment/data/services/AppointmentServiceImplTest.java
+++ doctorsappointment-data/src/test/java/com/example/riad/doctorsappointment/data/services/AppointmentServiceImplTest.java
@@ -1,12 +1,23 @@
 package com.example.riad.doctorsappointment.data.services;
 
+import com.example.riad.doctorsappointment.data.domains.AppointmentBook;
 import com.example.riad.doctorsappointment.data.repos.AppointmentRepository;
-import com.example.riad.doctorsappointment.data.services.Interfaces.AppointmentService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -16,14 +27,53 @@ public class AppointmentServiceImplTest {
     AppointmentRepository appointmentRepository;
 
     @InjectMocks
-    AppointmentService appointmentService;
+    AppointmentServiceImpl appointmentService;
 
     @Test
     public void getAllAppointments() {
-        appointmentService.getAllAppointments();
+        AppointmentBook appointmentBook = new AppointmentBook();
+        List<AppointmentBook> appointmentBooks = new ArrayList<>();
+
+        appointmentBooks.add(appointmentBook);
+
+        when(appointmentRepository.findAll()).thenReturn(appointmentBooks);
+
+        List<AppointmentBook> foundAppointmentBooks = appointmentService.getAllAppointments();
+
+        verify(appointmentRepository).findAll();
+
+        assertThat(foundAppointmentBooks).hasSize(1);
     }
 
     @Test
     public void addAppointment() {
+        AppointmentBook appointmentBook = new AppointmentBook();
+
+        when(appointmentRepository.save(any(AppointmentBook.class))).thenReturn(appointmentBook);
+
+        AppointmentBook savedAppointmentBook = appointmentService.addAppointment(new AppointmentBook());
+
+        verify(appointmentRepository).save(any(AppointmentBook.class));
+
+        assertThat(savedAppointmentBook).isNotNull();
+    }
+
+
+    @Test
+    void findById() {
+        AppointmentBook appointmentBook = new AppointmentBook();
+
+        when(appointmentRepository.findById(anyLong())).thenReturn(Optional.of(appointmentBook));
+
+        AppointmentBook savedAppointmentBook = appointmentService.findById(1L).get();
+
+        verify(appointmentRepository).findById(anyLong());
+
+        assertThat(savedAppointmentBook).isNotNull();
+
+    }
+
+    @Test
+    void updateAppointment() {
     }
 }
